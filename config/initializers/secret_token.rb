43b67e8e75424d1428b3cf2a9ad4814f24879ee3
Fiyah Damage <fireras@gmail.com>
2012-10-08 20:36:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wir2::Application.config.secret_token = '8882e8925bf2bb05cf6f5b04377d8f24856408a5251e05dca5f95d060d961ea5718a168f50e47ee2f5a7ab79e085f93b1c4f51ef35fa8a13dab31f3c5177668c'
