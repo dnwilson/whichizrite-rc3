fe7cbdc78f6e9f2393b6d783fc5d32139db922ee
Fiyah Damage <fireras@gmail.com>
2012-12-15 19:33:50 -0500

    Remove old Relationship MVC

--- db/migrate/20121211214753_add_hide_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddHideColumnToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :hide, :boolean, :default => false
-  end
-end
--- db/migrate/20121213040547_add_private_user_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPrivateUserToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :private_user, :boolean, :default => false
-  end
-end
